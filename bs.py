0ec154083fb270ff4a2bc582b52cccec2c8fd6b8
Michael Cohn <contact@mcohn.net>
2018-03-23 20:45:37 -0700

    added checking for some kinds of non-tweet content that should be discarded

--- bs.py
+++ bs.py
@@ -1,50 +1,41 @@
+import csv
 import pdb
 import os
 from bs4 import BeautifulSoup as bs
-
-def extractTexts(entities):
-    return [i.get_text().strip() for i in entities]
-
-class pooter:
-    def get():
-        return ""
-    def get_text():
-        return ""
+from types import NoneType
 
 
-
-
-home = os.path.expanduser("~")
-os.chdir(home+"/Google Drive/storify")
+#home = os.path.expanduser("~")
+#os.chdir(home+"/Google Drive/storify")
 ht = open("input_twitter.html", 'r')
 soup = bs(ht, 'html.parser')
 
 tweets = soup.find_all("div", {"class":"content"})
 tweetData = []
 
+def isNonTweet(content):
+    test = content.find("a", {"class":"js-recommend-link"})
+    return test is not None
+
 for tweet in tweets:
     tweetDat = {}
+    if isNonTweet(tweet):
+        continue
     try:
-        tweetDat['name'] = soup.find("span", {"class": "FullNameGroup"}).get_text()
+        tweetDat['name'] = tweet.find("span", {"class": "FullNameGroup"}).get_text()
     except AttributeError:
         tweetDat['name'] = ""
     try:
-        tweetDat['handle'] = soup.find("span", {"class":"username"}).get_text()
+        tweetDat['handle'] = tweet.find("span", {"class":"username"}).get_text()
     except AttributeError:
         tweetDat['handle'] = ""
     try:
-        tweetDat['link'] = soup.find("a", {"class":"tweet_timestamp"}).get("href")
+        tweetDat['link'] = tweet.find("a", {"class":"tweet_timestamp"}).get("href")
     except AttributeError:
         tweetDat['link'] = ""
     try:
-        tweetDat['time'] = soup.find("span", {"class":"_timestamp"}).get("data-time")
+        tweetDat['time'] = tweet.find("span", {"class":"_timestamp"}).get("data-time")
     except AttributeError:
-        tweetDat['link'] = ""
-
-
-    for tweet_time in tweet_times:
-        tweet_timestamps.append(tweet_time.get("data-time"))
-        tweet_timestamps.append()
-
-
-pdb.set_trace()
+        tweetDat['time'] = ""
+    print(tweetDat)
+    pdb.set_trace()
